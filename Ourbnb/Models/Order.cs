5d14f65567c9132d9b19997eb13393c07d260f2a
Hamza Farah <89578136+HamzaF02@users.noreply.github.com>
2023-10-09 15:52:35 +0200

    a

---
+++ Ourbnb/Models/Order.cs
@@ -0,0 +1,17 @@
+﻿using MyShop.Models;
+
+namespace Ourbnb.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; }
+        public int RentalId { get; set; }
+        public Rental Rental { get; set;}
+        public int totalPrice { get; set; }
+        public DateOnly From { get; set; }
+        public DateOnly To { get; set; }
+        public int Rating { get; set; }
+    }
+}
--- Ourbnb/Models/Rental.cs
+++ Ourbnb/Models/Rental.cs
@@ -7,14 +7,17 @@ namespace Ourbnb.Models
         public int RentalId { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
-        public DateTime FromDate { get; set; }
-        public DateTime ToDate { get; set; }
+        public DateOnly FromDate { get; set; }
+        public DateOnly ToDate { get; set; }
         public double Rating { get; set; }
         public string Location { get; set; }
         public int Price { get; set; }
+        public List<string> Bilder { get; set; }
 
         public int OwnerId { get; set; }
         public Customer Owner { get; set; }
+
+
 
     }
 }
