5912baafecd536a73506fe557372575cbc2dd2da
Hamza Farah <89578136+HamzaF02@users.noreply.github.com>
2023-10-10 14:59:08 +0200

    a

---
+++ Ourbnb/DAL/IRentalRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Ourbnb.DAL
+{
+	public class IRentalRepository
+	{
+		public IRentalRepository()
+		{
+		}
+	}
+}
+
--- Ourbnb/DAL/RentalDbContext.cs
+++ Ourbnb/DAL/RentalDbContext.cs
@@ -1,11 +1,17 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using Ourbnb.Models;
 namespace Ourbnb.DAL
 {
-	public class ItemDbContext
+	public class RentalDbContext : DbContext
 	{
-		public ItemDbContext()
+		public RentalDbContext(DbContextOptions<RentalDbContext> options) : base(options)
 		{
+			Database.EnsureCreated();
 		}
+		public DbSet<Rental> Rentals { get; set; }
+		public DbSet<Customer> Customers { get; set; }
+		public DbSet<Order> Orders { get; set; }
 	}
 }
 
