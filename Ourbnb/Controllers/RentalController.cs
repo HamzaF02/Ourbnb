6a1e0dfa02fd36184b7fdb3e0f5f719f1b766152
Hamza Farah <89578136+HamzaF02@users.noreply.github.com>
2023-10-13 16:37:49 +0200

    iyg

--- Ourbnb/Controllers/RentalController.cs
+++ Ourbnb/Controllers/RentalController.cs
@@ -1,14 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Ourbnb.DAL;
+using Ourbnb.Models;
 
 namespace Ourbnb.Controllers
 {
     public class RentalController : Controller
     {
         private readonly RentalDbContext _rentalDbContext;
+
+        public RentalController(RentalDbContext rentalDbContext)
+        {
+            _rentalDbContext = rentalDbContext;
+        }
         public IActionResult Rentalspage()
         {
-            return View();
+            List<Rental> rentals = _rentalDbContext.Rentals.ToList();
+            return View(rentals);
         }
     }
 }
