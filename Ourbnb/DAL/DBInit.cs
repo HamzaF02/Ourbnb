fb008013df4a3758ad1db5f66ac42de4efc6fa34
Harkirat Singh <harkirat2705@gmail.com>
2023-10-16 15:43:50 +0200

    adding more rentals

--- Ourbnb/DAL/DBInit.cs
+++ Ourbnb/DAL/DBInit.cs
@@ -10,8 +10,6 @@ namespace Ourbnb.DAL
 		{
             using var serviceScope = app.ApplicationServices.CreateScope();
             RentalDbContext context = serviceScope.ServiceProvider.GetRequiredService<RentalDbContext>();
-            context.Database.EnsureDeleted();
-            context.Database.EnsureCreated();
 
             if (!context.Customers.Any())
             {
@@ -50,12 +48,48 @@ namespace Ourbnb.DAL
                         Name="Hytte i Nordkapp",
                         Location="Nordkapp",
                         Description="Fin moderne hytte med mye plass",
-                        Price=2000,
+                        Price=2500,
                         OwnerId=1,
-                        FromDate=new DateOnly(2023, 10, 30),
-                        ToDate=new DateOnly(2023, 10, 21),
+                        FromDate=new DateOnly(2023, 10, 21),
+                        ToDate=new DateOnly(2023, 10, 30),
                         Bilder= "https://hellvikhytte.no/content/uploads/sites/14/2021/08/Fjellerke-3-scaled.jpg",
                         Rating=4.5,
+                    },
+                    new Rental
+                    {
+                        Name = "House by the waterfalls",
+                        Description = "A nice place to be with friends and family to enjoy a calm and nice view",
+                        FromDate = new DateOnly(2015, 10, 10),
+                        ToDate = new DateOnly(2015, 10, 20),
+                        Rating = 3.5,
+                        Location = "Gran",
+                        Price = 150,
+                        Bilder = "/images/1.jpeg",
+                        OwnerId = 2,
+                    },
+                    new Rental
+                    {
+                        Name = "House by the forrest",
+                        Description = "A nice place to be with friends and family to enjoy a calm and nice view",
+                        FromDate = new DateOnly(2016, 04, 10),
+                        ToDate = new DateOnly(2016, 04, 20),
+                        Rating = 3.9,
+                        Location = "Tomter",
+                        Price = 150,
+                        Bilder = "/images/modernhouse.jpg",
+                        OwnerId = 1
+                    },
+                    new Rental
+                    {
+                        Name = "House by the mountains",
+                        Description = "A nice place to be with friends and family to enjoy a calm and nice view",
+                        FromDate = new DateOnly(2019, 08, 10),
+                        ToDate = new DateOnly(2019, 08, 10),
+                        Rating = 4.1,
+                        Location = "Hafjell",
+                        Price = 150,
+                        Bilder = "/images/mountain.jpeg",
+                        OwnerId = 2
                     }
                 };
                 context.AddRange(rentals);
