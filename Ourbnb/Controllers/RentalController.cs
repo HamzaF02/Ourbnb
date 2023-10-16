2d80d7fbb3fcbaf11a9b3964d594f053ef1ea0a6
Ylligg <yll.gashi@hotmail.com>
2023-10-16 14:34:25 +0200

    table og grid for rentals
    suck my small shrimp

--- Ourbnb/Controllers/RentalController.cs
+++ Ourbnb/Controllers/RentalController.cs
@@ -7,16 +7,101 @@ namespace Ourbnb.Controllers
 {
     public class RentalController : Controller
     {
-        private readonly RentalDbContext _rentalDbContext;
-
-        public RentalController(RentalDbContext rentalDbContext)
+      
+     
+        public IActionResult Table()
         {
-            _rentalDbContext = rentalDbContext;
-        }
-        public IActionResult Rentalspage()
-        {
-            List<Rental> rentals = _rentalDbContext.Rentals.ToList();
+            var rentals = GetRentals();
+            ViewBag.CurrentViewName = "Table";
             return View(rentals);
+        }
+
+        public IActionResult Grid()
+        {
+            var rentals = GetRentals();
+            ViewBag.CurrentViewName = "Grid";
+            return View(rentals);
+        }
+
+        public List<Rental> GetRentals()
+        {
+            var rentals = new List<Rental>();
+            var owner1 = new Customer
+            {
+                CustomerId = 1,
+                FirstName = "A",
+                LastName = "B",
+                Address = "C",
+                Phone = 122,
+                Email = "Gmail"
+            };
+
+
+            var rental1 = new Rental
+            {
+                RentalId = 1,
+                Name = "House by the waterfalls",
+                Description = "A nice place to be with friends and family to enjoy a calm and nice view",
+                FromDate = new DateOnly(2015, 10, 10),
+                ToDate = new DateOnly(2015, 10, 20),
+                Rating = 3.5,
+                Location = "Gran",
+                Price = 150,
+                Bilder = {},
+                OwnerId = 1,
+                Owner = owner1
+            };
+
+            var rental2 = new Rental
+            {
+                RentalId = 2,
+                Name = "House by the waterfalls",
+                Description = "A nice place to be with friends and family to enjoy a calm and nice view",
+                FromDate = new DateOnly(2015, 10, 10),
+                ToDate = new DateOnly(2015, 10, 10),
+                Rating = 3.5,
+                Location = "Gran",
+                Price = 150,
+                Bilder = {},
+                OwnerId = 1,
+                Owner = owner1
+            };
+
+            var rental3 = new Rental
+            {
+                RentalId = 3,
+                Name = "House by the waterfalls",
+                Description = "A nice place to be with friends and family to enjoy a calm and nice view",
+                FromDate = new DateOnly(2015, 10, 10),
+                ToDate = new DateOnly(2015, 10, 20),
+                Rating = 3.5,
+                Location = "Gran",
+                Price = 150,
+                Bilder = { },
+                OwnerId = 1,
+                Owner = owner1
+            };
+
+            var rental4 = new Rental
+            {
+                RentalId = 4,
+                Name = "House by the waterfalls",
+                Description = "A nice place to be with friends and family to enjoy a calm and nice view",
+                FromDate = new DateOnly(2015, 10, 10),
+                ToDate = new DateOnly(2015, 10, 10),
+                Rating = 3.5,
+                Location = "Gran",
+                Price = 150,
+                Bilder = { },
+                OwnerId = 1,
+                Owner = owner1
+            };
+
+            rentals.Add(rental1);
+            rentals.Add(rental2);
+            rentals.Add(rental3);
+            rentals.Add(rental4);
+            return rentals;
         }
     }
 }
