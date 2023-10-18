5b94e0f1d2a49cc28afd43e8df34e6bc83f4f850
ylligashi <yll.gashi@hotmail.com>
2023-10-18 15:35:32 +0200

    lagde customerRepo cuz det hadde ikke

---
+++ Ourbnb/ViewModels/listRental.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Ourbnb.Models;
+
+namespace MyShop.ViewModels
+{
+    public class listRental
+    {
+        public IEnumerable<Rental> Items;
+        public string? CurrentViewName;
+
+        public listRental(IEnumerable<Rental> items, string? currentViewName)
+        {
+            Items = items;
+            CurrentViewName = currentViewName;
+        }
+    }
+}
