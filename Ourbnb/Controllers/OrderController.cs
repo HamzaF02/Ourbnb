95fad36b622598f4b8e3ad52bdb2565ebb62464c
IbbiSecka <s355423@oslomet.no>
2023-10-18 13:26:23 +0200

    Your orders er clickable

---
+++ Ourbnb/Controllers/OrderController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Ourbnb.Models;
+
+namespace Ourbnb.Controllers;
+
+    public class OrderController : Controller
+    {
+        public IActionResult Table()
+        {
+            List<Order> Orders = new List<Order>(); 
+            return View(Orders);
+        }
+    }
+
