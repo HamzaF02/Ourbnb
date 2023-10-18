a2b621f21eb61ee40f8571f4816ebbaa09d6be9e
ylligashi <yll.gashi@hotmail.com>
2023-10-18 13:23:29 +0200

    register

---
+++ Ourbnb/Controllers/RegisterController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Ourbnb.Models;
+using System.Diagnostics;
+
+namespace Ourbnb.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private readonly ILogger<RegisterController> _logger;
+
+        public RegisterController(ILogger<RegisterController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult regPage()
+        {
+            return View();
+        }
+
+       }
+}
