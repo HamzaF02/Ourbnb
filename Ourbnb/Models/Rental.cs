bb3b0abccdc6cb26e62ba48b88a88e98375d1b76
ylligashi <yll.gashi@hotmail.com>
2023-10-19 23:13:49 +0200

    vet ikke hva som skjer med appen

--- Ourbnb/Models/Rental.cs
+++ Ourbnb/Models/Rental.cs
@@ -5,12 +5,18 @@ namespace Ourbnb.Models
     public class Rental
     {
         public int RentalId { get; set; }
+
+        [RegularExpression(@"[0-9a-zA-ZæøåÆØÅ]{1,30}", ErrorMessage = "Name must be letters or numbers between 1 to 30 charachters")]
         public string Name { get; set; } = string.Empty;
+        [StringLength(200)]
         public string Description { get; set; } = string.Empty;
         public DateOnly FromDate { get; set; }
         public DateOnly ToDate { get; set; }
+        [Range(0.01, 5, ErrorMessage ="Rating must be greater than 0 and 5 or less")]
         public double? Rating { get; set; } = default!;
+        [RegularExpression(@"[0-9a-zA-ZæøåÆØÅ]{1,50}", ErrorMessage = "Name must be letters or numbers between 1 to 50 charachters")]
         public string Location { get; set; } = string.Empty;
+        [Range(0.01, int.MaxValue, ErrorMessage = "Price must be greater than 0 ")]
         public int Price { get; set; }
         public string Bilder { get; set; } = string.Empty;
         public int OwnerId { get; set; }
