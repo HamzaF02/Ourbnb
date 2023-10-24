009a9a74608b9cfc2dfd88ded86b3d634addc1e6
IbbiSecka <s355423@oslomet.no>
2023-10-24 14:27:41 +0200

    h

---
+++ Ourbnb/Controllers/OrdersController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Ourbnb.DAL;
+using Ourbnb.Models;
+using System.Diagnostics;
+
+namespace Ourbnb.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private readonly ILogger<OrdersController> _logger;
+        private readonly IRepository<Order> _repository;
+
+        public OrdersController(ILogger<OrdersController> logger, IRepository<Order> repository)
+        {
+            _logger = logger;
+            _repository = repository;
+        }
+
+        public async Task<IActionResult> ListofOrders()
+        {
+            var orders = await _repository.GetAll();
+            return View(orders);
+        }
+
+    }
+
+}
+
