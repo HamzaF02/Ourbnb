74353dcb35928496ddad55dd44c49959fffd07bf
Hamza Farah <89578136+HamzaF02@users.noreply.github.com>
2023-10-27 19:41:56 +0200

    a

---
+++ Ourbnb/Migrations/20231027172355_ChangedModels.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ourbnb.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangedModels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Rentals",
+                type: "TEXT",
+                maxLength: 200,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 200);
+
+            migrationBuilder.AddColumn<string>(
+                name: "IdentityId",
+                table: "Rentals",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "IdentityId",
+                table: "Customers",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IdentityId",
+                table: "Rentals");
+
+            migrationBuilder.DropColumn(
+                name: "IdentityId",
+                table: "Customers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Rentals",
+                type: "TEXT",
+                maxLength: 200,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 200,
+                oldNullable: true);
+        }
+    }
+}
