fce4fef1e3320e312221d16e72908c622b26bfc2
Hamza Farah <89578136+HamzaF02@users.noreply.github.com>
2023-10-30 22:27:08 +0100

    a

--- Ourbnb/DAL/RentalRepository.cs
+++ Ourbnb/DAL/RentalRepository.cs
@@ -14,15 +14,18 @@ namespace Ourbnb.DAL
             _db = db;
             _logger = logger;
         }
-
+        //Creation of a Rental
         public async Task<bool> Create(Rental rental)
         {
+            //Try catch in case of error
             try { 
+                //Adds it to database and saves
                 _db.Rentals.Add(rental);
                 await _db.SaveChangesAsync();
                 return true;
             }catch (Exception ex)
             {
+                //logs and returns false if something goes wrong
                 _logger.LogError("[RentalRepository] rental creation failed for rental {@rental}, error message: {ex}", rental, ex.Message);
                 return false;
             }
