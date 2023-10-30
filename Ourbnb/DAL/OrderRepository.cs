ac1927d18540a44bfdf17700122f2f0507dffbf0
ylli <s364574@oslomet.no>
2023-10-31 00:45:55 +0100

    order comments

--- Ourbnb/DAL/OrderRepository.cs
+++ Ourbnb/DAL/OrderRepository.cs
@@ -10,42 +10,52 @@ namespace Ourbnb.DAL
 
         private readonly ILogger<OrderRepository> _logger;
 
+        // constructor for database and logger
         public OrderRepository(RentalDbContext db, ILogger<OrderRepository> logger)
         {
             _db = db;
             _logger = logger;
         }
 
+        // function to create a new order 
         public async Task<bool> Create(Order order)
         {
+            // try to add order to database
             try
             {
+                // add order to database and save changes 
                 _db.Orders.Add(order);
                 await _db.SaveChangesAsync();
                 return true;
             }catch (Exception ex)
             {
+                // failed to add order
                 _logger.LogError("[OrderRepository] order creation failed for order {@order}, error message: {ex}", order, ex.Message);
                 return false;
             }
         }
 
+        // function to delete a order 
         public async Task<bool> Delete(int id)
         {
+            // try to delete order to database
             try
             {
+                // find order
                 var order = await _db.Orders.FindAsync(id);
                 if (order == null)
                 {
+                    // error if order is not found
                     _logger.LogError("[OrderRepository] order not found for the OrderId {OrderId:0000}", id);
                     return false;
                 }
-
+                // remove order from databse
                 _db.Orders.Remove(order);
                 await _db.SaveChangesAsync();
                 return true;
             }catch(Exception ex)
             {
+                // 
                 _logger.LogError("[OrderRepository] order deletion failed for OrderId {OrderId:0000}, error message: {ex}", id, ex.Message);
                 return false;
             }
